cf435160c21a685be6665f3699258b9342f70e89
Erick Marcelo <Ems2201@outlook.com>
2020-05-20 14:25:40 -0300

    Testando a exceção personalizada de tabuleiro

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
-
-namespace Xadrez
+using tabuleiro;
+using xadrez;
+namespace xadrez_console
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Tabuleiro tab = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro ta = new Tabuleiro(8, 8);
+
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+
+                Tela.imprimirTabuleiro(tab);
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
         }
     }
 }
