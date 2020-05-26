85c28ef6b54fbf3857d67d50bdf3ad386633fbe6
Erick Marcelo <Ems2201@outlook.com>
2020-05-26 11:01:51 -0300

    Consertando o Programa pois o hd externo o bugou

--- Xadrez/Tela.cs
+++ Xadrez/Tela.cs
@@ -9,34 +9,40 @@ namespace xadrez_console
         public static void imprimirTabuleiro(Tabuleiro tab)
         {
 
-            for (int i = 0; i < tab.linhas; i++){
+            for (int i = 0; i < tab.linhas; i++)
+            {
                 Console.Write(8 - i + " ");
-                for (int j = 0; j < tab.colunas; j++){
-                    if (tab.peca(i, j) == null){
-                        Console.Write("-  ");
+                for (int j = 0; j < tab.colunas; j++)
+                {
+                    if (tab.peca(i, j) == null)
+                    {
+                        Console.Write("- ");
                     }
                     else
                     {
-                        Tela.imprimirPeca(tab.peca(i, j));
+                        imprimirPeca(tab.peca(i, j));
                         Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
             }
-            Console.WriteLine(" a  b  c  d  e  f  g  h ");
+            Console.WriteLine("  a b c d e f g h");
         }
-            public static void imprimirPeca(Peca peca)
+
+        public static void imprimirPeca(Peca peca)
         {
-            if(peca.cor == Cor.Branca) {
+            if (peca.cor == Cor.Branca)
+            {
                 Console.Write(peca);
             }
-        else
+            else
             {
                 ConsoleColor aux = Console.ForegroundColor;
-                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.Write(peca);
                 Console.ForegroundColor = aux;
             }
         }
+
     }
 }
