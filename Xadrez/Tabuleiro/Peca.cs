c727c2450f1bf742bee95bbe8c69718306fb425a
Erick Marcelo <Ems2201@outlook.com>
2020-05-26 11:34:03 -0300

    Fazendo testes de movimentação

--- Xadrez/Tabuleiro/Peca.cs
+++ Xadrez/Tabuleiro/Peca.cs
@@ -2,21 +2,23 @@
 {
     class Peca
     {
+
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
         public int qteMovimentos { get; protected set; }
         public Tabuleiro tab { get; protected set; }
 
-        public Peca(Posicao posicao, Tabuleiro tab, Cor cor)
+        public Peca(Tabuleiro tab, Cor cor)
         {
-            this.posicao = posicao;
-        }  
-            public Peca(Tabuleiro tab, Cor cor)
-            {
-                this.posicao = null;
-                this.tab = tab;
-                this.cor = cor;
-                this.qteMovimentos = 0;
-            }
+            this.posicao = null;
+            this.tab = tab;
+            this.cor = cor;
+            this.qteMovimentos = 0;
+        }
+
+        public void incrementarQteMovimentos()
+        {
+            qteMovimentos++;
         }
     }
+}
